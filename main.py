f0caec10c991ac374cfc2f67f533f4b8d5cd11fc
12felixXItkjsmeksa-prog <felix.tkjsmeksa25@gmail.com>
2026-09-02 09:55:56 +0700

    main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from ganjilgenap import cek_ganjil_genap
+
+print("=== PROGRAM CEK GANJIL GENAP ===")
+
+angka = int(input("Masukkan angka: "))
+
+hasil = cek_ganjil_genap(angka)
+
+print(f"Hasil: {angka} adalah bilangan {hasil}")
