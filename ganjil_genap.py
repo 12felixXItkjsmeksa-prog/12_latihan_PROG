a523ac18ed888f6a098843233879a5ee87963213
12felixXItkjsmeksa-prog <felix.tkjsmeksa25@gmail.com>
2026-09-07 13:35:45 +0700

    Update ganjil_genap.py

--- ganjil_genap.py
+++ ganjil_genap.py
@@ -3,15 +3,3 @@ def cek_ganjil_genap(angka):
         print(f"{angka} adalah bilangan genap")
     else:
         print(f"{angka} adalah bilangan ganjil")
-
-
-while True:
-    angka = int(input("Masukkan sebuah angka: "))
-
-    cek_ganjil_genap(angka)
-
-    pilihan = input("Apakah ingin lanjut y/n: ").lower()
-
-    if pilihan == 'n':
-        print("Program selesai. Terima kasih.")
-        break
